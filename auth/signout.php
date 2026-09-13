627ad40d04b80f49e335e8c899c998fe3b10a0bc
Aizachan <janlayaiza@gmail.com>
2026-09-13 18:21:19 +0800

    added signout.php

--- auth/signout.php
+++ auth/signout.php
@@ -1,9 +1,10 @@
-<?php>
-require_once __DIR__ . '/../config/config.php';
+<?php
+require_once __DIR__ . '/../../../config/config.php';
 
 $_SESSION = [];
 session_destroy();
 
 header('Location: ' . BASE_URL . '/index.php');
 exit;
+
 ?>
