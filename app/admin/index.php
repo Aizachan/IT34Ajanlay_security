47c6fd2f44623303a495b78dd9e753d713ff553f
Aizachan <janlayaiza@gmail.com>
2026-09-15 05:11:42 +0800

    fixed code but not complete

--- app/admin/index.php
+++ app/admin/index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -9,4 +9,23 @@
      <h1>hello Admin</h1>
      <a href="../auth/signout.php">Sign Out</a>
 </body>
-</html>
+</html> -->
+<?php
+
+require_once __DIR__ . '/../../config/config.php';
+require_once __DIR__ . '/../../config/functions.php';
+
+requireRole('admin');
+
+?>
+
+<h1>Hello Admin</h1>
+
+<p>
+    Welcome,
+    <?= htmlspecialchars($_SESSION['user_username']) ?>
+</p>
+
+<a href="<?= BASE_URL ?>auth/signout.php">
+    Sign Out
+</a>
